698381c0c4c3c9d8e0713eafdbc83d7607c9d6fa
DreamSoul <diao_em@hotmail.com>
2015-03-23 01:30:36 -0400

    General Structure OK and function, but not as expected, need to change the lowpass frame size and some other parameters

--- main.m
+++ main.m
@@ -1,13 +1,13 @@
 %% Emprical Constants
 clc
 clear all
-Fs = 8000;                       %Sampling Rate
+Fs = 8000;                       %sampling Rate
 F0min = 50;                      %minimum F0 to search for (HZ)
 F0max = 500;                     %maximum F0 to search for (HZ)
-Fds = Fs/(round(Fs/(4*F0max)));  %Low sampling rate
-t = .01;                         %Frame advance Duration
-w = .0075;                       %Analysis window Duration
-CAND_TR = 0.3;                   % minimum acceptable peak value
+Fds = Fs/(round(Fs/(4*F0max)));  %low sampling rate
+t = .01;                         %frame advance Duration
+w = .0075;                       %analysis window Duration
+CAND_TR = 0.3;                   %minimum acceptable peak value
 N_CANDS = 20;                    %max number of hypothese(candidates) at each frame
 A_FACT = 10000;                  %unvoiced detection, term to decrease peaks of weak signals
 % Constatnts for dynamic programming pitch selection
@@ -17,18 +17,22 @@ VTRAN_C = 0.005;                 %fixed voicing-state transition cost
 VTR_A_C = 0.5;                   %delta amplitude modulated tranistion cost
 VTR_S_C = 0.5;                   %delta spectrum modulated transition cost
 VO_BIAS = 0.0;                   %bias to encourage voiced hypotheses
-DOUBLE_C = 0.35;                  %cost of exact F0 doubling or halving
+DOUBLE_C = 0.35;                 %cost of exact F0 doubling or halving
+preemph = -7000;                 %s-plane position of preemphasis zero      
+nfullag = 7;                     %number of full lags to try (must be odd)
 %% Derived Constants
-T = 1/Fs;                       %Sampling Interval for Fs
-z = round(t/T);                %Frame step size for Fs(samples)
-n = round(w/T);                %Analysis window length for Fs(samples), number of samples correlated at each lag 
-K = round(Fs/F0min);            %Max Lags, longeset lag at each frame for Fs
-Kmin = round(Fs/F0max);         %Min Lags, shortest lag at each frame for Fs
+T = 1/Fs;                        %sampling Interval for Fs
+z = round(t/T);                  %frame step size for Fs(samples)
+n = round(w/T);                  %analysis window length for Fs(samples), number of samples correlated at each lag 
+K = round(Fs/F0min);             %max Lags, longeset lag at each frame for Fs
+Kmin = round(Fs/F0max);          %min Lags, shortest lag at each frame for Fs
 
-Kdmax = round(Fds/F0min);        %Max Lags, longeset lag at each frame for Fds
-Kdmin = round(Fds/F0max);        %Min Lags, shortest lag at each frame for Fds
+Kdmax = round(Fds/F0min);        %max Lags, longeset lag at each frame for Fds
+Kdmin = round(Fds/F0max);        %min Lags, shortest lag at each frame for Fds
 
-
+J = round(0.03*Fs);              %window spacing for rms measurement
+h = round(0.02*Fs);              %window spacing for rms measurement
+lpcord = 2+round(Fs/1000);       %lpc order for itakura distance
 %% Signal Generation
 %y=0.4*chirp(time,200,Duration,400,'li');
 %y = 0.4*sawtooth(2*pi*440*time);
@@ -38,10 +42,10 @@ y=resample(yh,Fs,fs);
 time=(0:length(y)-1)/Fs;
 Duration = time(end);
 M = round(length(time)/z);     %Number of Frames
-subplot(2,1,1)
+subplot(3,1,1)
 plot(time,y)
 
-%% NCCF LOWPASS
+%% NCCF LowPass
 R = zeros(K,M);
 Peaks = zeros(N_CANDS,M);
 Locations = zeros(N_CANDS,M);
@@ -77,11 +81,13 @@ for i=0:M-1
     Peaks(1:length(p),i+1) = p;
     Locations(1:length(loc),i+1) = loc;
 end
-%% NCCF HIGHPASS
+%% NCCF HighPass
+RL = R;
 R = zeros(K,M);
 High_Peaks = zeros(N_CANDS,M);
 High_Locations = zeros(N_CANDS,M);
 yh = yh - mean(yh);
+Ii = struct('Cij',{},'Lij',{},'dij',{},'diI',{},'Li_1k',{},'deltaijk',{},'Dij',{},'cost',{},'prev',{});
 for i=0:M-1
     for j=i*z+1:i*z+n
         e_m = sum(yh(i*z+1:i*z+n).^2);
@@ -96,7 +102,7 @@ for i=0:M-1
                   break;
                end
               e_mpk = sum(yh(i*z+1+k:i*z+n+k).^2);
-              R(k,i+1)=R(k,i+1)+(yh(j)*yh(j+k)/sqrt(A_FACT+e_m*e_mpk));
+              R(k,i+1)=R(k,i+1)+(yh(j)*yh(j+k)/sqrt(e_m*e_mpk));
             end
         end
     end
@@ -108,9 +114,9 @@ for i=0:M-1
     end
     p = p(p >CAND_TR*max_peak);
     loc = loc(p>CAND_TR*max_peak);
+    [p,loc_sort] = sort(p,'descend');
+    loc = loc(loc_sort);
     if (length(p) > N_CANDS-1)
-        [p,loc_sort] = sort(p,'descend');
-        loc = loc(loc_sort);
         p = p(1:N_CANDS-1);
         loc = loc(1:N_CANDS-1);
     end
@@ -119,31 +125,66 @@ for i=0:M-1
     High_Peaks(1:length(p),i+1) = p;
     High_Locations(1:length(loc),i+1) = loc;
     %% Peak Selection
-    deltaijk = cell(M);
     beta = LAG_WT/(Fs/F0min);
-    Cij = p;
-    Lij = loc;
-    dij = 1-Cij.*(1-beta.*Lij);     %objective function as the local cost for frame i is voiced with period TLij
-    diI = VO_BIAS + max(Cij);       %cost for the single unvoiced hypothesis at this frame   
-    if (i==0)
-        deltaijk(i)=0;
-    elseif (any(High_Locations(1:length(loc),i)~=0)&&(any(p~=0)))  %current adn previous frames are both voiced
-        Li_1k = High_Locations(1:length(loc),i);
-        Li_1k(Li_1k==0) = [];
-        epsilonjk = abs(log(respecdiv(Lij,Li_1k)));     
-        
-        deltaijk(i) = FREQ_WT*min(epsilonjk,DOUBLE_C+abs(epsilonjk-log(2.0))); %inter-frame F0 transitioin cost delta at frame i
-    end
+    Ii(i+1).Cij = p;                                                    %value of the jth local maximum in Peaks at frame i
+    Ii(i+1).Lij = loc;                                                  %sample lag at which Cij was observed
+    Ii(i+1).dij = 1-Ii(i+1).Cij.*(1-beta.*Ii(i+1).Lij);                 %objective function as the local cost for frame i is voiced with period TLij
+    Ii(i+1).diI = VO_BIAS + max(Ii(i+1).Cij);                           %cost for the single unvoiced hypothesis at this frame
+    if (i+1==1)                                                         %the first frame
+        Ii(i+1).deltaijk = 0;                                             %inter-frame F0 transitioin cost delta at frame i
+        costm=zeros(1,length(Ii(i+1).Lij)+1);
+        Ii(i+1).Dij = 0;
+    else
+        costm=zeros(length(Ii(i).Lij)+1,length(Ii(i+1).Lij)+1);
+        if (any(High_Locations(:,i))&&(any(High_Locations(:,i+1))))        %current and previous frames are both voiced
+            epsilonjk=abs(log(repmat(Ii(i+1).Lij',length(Ii(i).Lij),1)./repmat(Ii(i).Lij,1,length(Ii(i+1).Lij))));
+            Ii(i+1).deltaijk = FREQ_WT*min(epsilonjk,DOUBLE_C+abs(epsilonjk-log(2.0)));
+            costm(2:end,2:end)=Ii(i+1).deltaijk;
+        else
+            rmsidx = i*z+1+(1:J)+floor((h-z)/2);
+            rmsidx_h = rmsidx-h;
+            rmswin = hann(J);
+            rri=sqrt((rmswin(rmsidx_h>0)'*yh(rmsidx(rmsidx_h>0)).^2)/(rmswin(rmsidx_h>0)'*yh(rmsidx_h(rmsidx_h>0)).^2));
+            sp=filter([1 exp(preemph/fs)],1,yh);                               %preemphasised speech for LPC calculation
+            Si=0.2/(distitar(lpcauto(sp(rmsidx(rmsidx_h>0)),lpcord),lpcauto(sp(rmsidx_h(rmsidx_h>0)),lpcord),'e')-0.8);
+            if (any(High_Locations(:,i))&&(~any(High_Locations(:,i+1))))       %voiced to unvoiced 
+                Ii(i+1).deltaijk = VTRAN_C + VTR_S_C*Si + VTR_A_C*rri;   
+                costm(2:end,1)= Ii(i+1).deltaijk;
+            elseif (~any(High_Locations(:,i))&&(any(High_Locations(:,i+1))))   %unvoiced to voiced
+                Ii(i+1).deltaijk = VTRAN_C + VTR_S_C*Si + VTR_A_C/rri;
+                costm(1,2:end) = Ii(i+1).deltaijk;
+            elseif (~any(High_Locations(:,i))&&(~any(High_Locations(:,i+1))))  %unvoiced to unvoiced
+                Ii(i+1).deltaijk = 0;
+            end
+        end
+        costall = [Ii(i).diI;Ii(i).dij];
+        if (isempty(costall))
+            costall = 0;
+        end
+        costm=costm+repmat(costall,1,length(Ii(i+1).Lij)+1);  % add in cumulative costs
+        [costi,previ]=min(costm,[],1);
+        Ii(i+1).cost = [Ii(i+1).diI;Ii(i+1).dij]'+ costi;
+        Ii(i+1).prev = previ; 
+    end                          
 end
+%% Trace Back
+best=zeros(1,M-1);
+[cbest,best(M-1)]=min(Ii(M-1).cost);
+for i=M-2:-1:1
+    best(i)=Ii(i+1).prev(best(i+1));
+end
+Lags =[];
+for i=1:M-1
+    Lags = [Lags High_Locations(best(i),i)];
+end
+F0 = Fs./Lags;
+F0(F0>F0max|F0<F0min|~isfinite(F0)) = 0;
 
-
-
-
-
-
-%%
-subplot(2,1,2)
-imagesc([0, Duration],[Kdmin,K],R)
+%% Plot
+subplot(3,1,2)
+imagesc([0, Duration],[Kdmin+1,K],RL)
 set(gca,'YDir','normal');
 colormap(flipud(colormap('gray')))
 colorbar('southoutside')
+subplot(3,1,3)
+plot(1:M-1,F0);
