4a60ff219f8c7a9b208e6c1744e6c11ba1c92e81
DreamSoul <diao_em@hotmail.com>
2015-04-28 03:00:08 -0400

    Five wave files performance evaluation done

--- src/GeneralTest.m
+++ src/GeneralTest.m
@@ -1,6 +1,66 @@
-reference_filename1 = './Sample/AlexanderRoss_GoodbyeBolero_STEM_06.csv';
-wave_filename1 = './Sample/AlexanderRoss_GoodbyeBolero_CLV.wav';
-Test(reference_filename1,wave_filename1);
-reference_filename2 = './Sample/BrandonWebster_YesSirICanFly_STEM_02.csv';
-wave_filename2 = './Sample/BrandonWebster_YesSirICanFly_CLV.wav';
-Test(reference_filename2,wave_filename2);
+set(0,'defaultfigurecolor', 'w')
+reference_filename1 = 'AlexanderRoss_GoodbyeBolero_STEM_06.csv';
+wave_filename1 = 'AlexanderRoss_GoodbyeBolero_CLV.wav';
+Sample_path1 = './Sample/AlexanderRoss_GoodbyeBolero/';
+Output_path1 = './Output/AlexanderRoss_GoodbyeBolero/';
+% output_reference = strcat(Output_path1,'Reference.txt');
+% output_yaapt = strcat(Output_path1,'YAAPT.txt');
+% output_rapt = strcat(Output_path1,'RAPT.txt');
+% output_myrapt = strcat(Output_path1,'MyRAPT.txt');
+Test(reference_filename1,wave_filename1,Sample_path1,Output_path1);
+% yaapt1 = melExEval(output_reference,output_yaapt,'Delimiter','\t','OutputType','struct');
+% rapt1 = melExEval(output_reference,output_rapt,'Delimiter','\t','OutputType','struct');
+% myrapt1 = melExEval(output_reference,output_myrapt,'Delimiter','\t','OutputType','struct');
+
+
+reference_filename2 = 'BrandonWebster_YesSirICanFly_STEM_02.csv';
+wave_filename2 = 'BrandonWebster_YesSirICanFly_CLV.wav';
+Sample_path2 = './Sample/BrandonWebster_YesSirICanFly/';
+Output_path2 = './Output/BrandonWebster_YesSirICanFly/';
+% output_reference = strcat(Output_path2,'Reference.txt');
+% output_yaapt = strcat(Output_path2,'YAAPT.txt');
+% output_rapt = strcat(Output_path2,'RAPT.txt');
+% output_myrapt = strcat(Output_path2,'MyRAPT.txt');
+Test(reference_filename2,wave_filename2,Sample_path2,Output_path2);
+% yaapt2 = melExEval(output_reference,output_yaapt,'Delimiter','\t','OutputType','struct');
+% rapt2 = melExEval(output_reference,output_rapt,'Delimiter','\t','OutputType','struct');
+% myrapt2 = melExEval(output_reference,output_myrapt,'Delimiter','\t','OutputType','struct');
+
+reference_filename3 = 'ClaraBerryAndWooldog_WaltzForMyVictims_STEM_05.csv';
+wave_filename3 = 'ClaraBerryAndWooldog_WaltzForMyVictims_CLV.wav';
+Sample_path3 = './Sample/ClaraBerryAndWooldog_WaltzForMyVictims/';
+Output_path3 = './Output/ClaraBerryAndWooldog_WaltzForMyVictims/';
+% output_reference = strcat(Output_path3,'Reference.txt');
+% output_yaapt = strcat(Output_path3,'YAAPT.txt');
+% output_rapt = strcat(Output_path3,'RAPT.txt');
+% output_myrapt = strcat(Output_path3,'MyRAPT.txt');
+Test(reference_filename3,wave_filename3,Sample_path3,Output_path3);
+% yaapt3 = melExEval(output_reference,output_yaapt,'Delimiter','\t','OutputType','struct');
+% rapt3 = melExEval(output_reference,output_rapt,'Delimiter','\t','OutputType','struct');
+% myrapt3 = melExEval(output_reference,output_myrapt,'Delimiter','\t','OutputType','struct');
+
+reference_filename4 = 'MusicDelta_Country1_STEM_05.csv';
+wave_filename4 = 'MusicDelta_Country1_CLV.wav';
+Sample_path4 = './Sample/MusicDelta_Country1/';
+Output_path4 = './Output/MusicDelta_Country1/';
+% output_reference = strcat(Output_path4,'Reference.txt');
+% output_yaapt = strcat(Output_path4,'YAAPT.txt');
+% output_rapt = strcat(Output_path4,'RAPT.txt');
+% output_myrapt = strcat(Output_path4,'MyRAPT.txt');
+Test(reference_filename4,wave_filename4,Sample_path4,Output_path4);
+% yaapt4 = melExEval(output_reference,output_yaapt,'Delimiter','\t','OutputType','struct');
+% rapt4 = melExEval(output_reference,output_rapt,'Delimiter','\t','OutputType','struct');
+% myrapt4 = melExEval(output_reference,output_myrapt,'Delimiter','\t','OutputType','struct');
+
+reference_filename5 = 'MusicDelta_Disco_STEM_04.csv';
+wave_filename5 = 'MusicDelta_Disco_CLV.wav';
+Sample_path5 = './Sample/MusicDelta_Disco/';
+Output_path5 = './Output/MusicDelta_Disco/';
+% output_reference = strcat(Output_path5,'Reference.txt');
+% output_yaapt = strcat(Output_path5,'YAAPT.txt');
+% output_rapt = strcat(Output_path5,'RAPT.txt');
+% output_myrapt = strcat(Output_path5,'MyRAPT.txt');
+Test(reference_filename5,wave_filename5,Sample_path5,Output_path5);
+% yaapt5 = melExEval(output_reference,output_yaapt,'Delimiter','\t','OutputType','struct');
+% rapt5 = melExEval(output_reference,output_rapt,'Delimiter','\t','OutputType','struct');
+% myrapt5 = melExEval(output_reference,output_myrapt,'Delimiter','\t','OutputType','struct');
--- src/Test.m
+++ src/Test.m
@@ -1,17 +1,19 @@
-reference_filename2 = './Sample/BrandonWebster_YesSirICanFly_STEM_02.csv';
-wave_filename2 = './Sample/BrandonWebster_YesSirICanFly_CLV.wav';
-[raw_time,F0] = importfile(reference_filename);
-[y,fs]=audioread(wave_filename);
-time = 0:0.01:length(y)/fs;
-F0_reference = interp1(raw_time,F0,time,'nearest');
-F0_reference(isnan(F0_reference)) = 0;
+function [Reference, YAAPT, RAPT, MyRAPT] = Test (reference_filename,wave_filename,Sample_path,Output_path)
+reference = strcat(Sample_path,reference_filename);
+wave = strcat(Sample_path,wave_filename);
+output_reference = strcat(Output_path,'Reference.txt');
+output_yaapt = strcat(Output_path,'YAAPT.txt');
+output_rapt = strcat(Output_path,'RAPT.txt');
+output_myrapt = strcat(Output_path,'MyRAPT.txt');
+[raw_time,F0_reference] = importfile(reference);
+[y,fs]=audioread(wave);
 
 %% YAAPT
 tic
 yd = downsample(y,4);
 Pitch = yaapt(yd,fs/4,1,[]);
 time_yaapt = 0:0.01:length(Pitch)*0.01-0.01;
-F0_yaapt = interp1(time_yaapt,Pitch,time,'nearest');
+F0_yaapt = interp1(time_yaapt,Pitch,raw_time,'nearest');
 F0_yaapt(isnan(F0_yaapt)) = 0;
 toc
 
@@ -20,7 +22,7 @@ tic
 fx = fxrapt(y,fs,'u');
 fx(isnan(fx)) = 0;
 time_rapt = 0:0.01:length(fx)*0.01-0.01;
-F0_rapt = interp1(time_rapt,fx,time,'nearest');
+F0_rapt = interp1(time_rapt,fx,raw_time,'nearest');
 F0_rapt(isnan(F0_rapt)) = 0;
 toc
 
@@ -28,32 +30,29 @@ toc
 tic
 [F0,~] = myrapt(y,fs);
 time_myrapt = 0:0.01:length(F0)*0.01-0.01;
-F0_myrapt = interp1(time_myrapt,F0,time,'nearest');
+F0_myrapt = interp1(time_myrapt,F0,raw_time,'nearest');
 F0_myrapt(isnan(F0_myrapt)) = 0;
 toc
 
 %% File output
-Reference = [time;F0_reference];
-YAAPT = [time; F0_yaapt];
-RAPT = [time; F0_rapt];
-MyRAPT = [time; F0_myrapt];
+Reference = [raw_time'; F0_reference'];
+YAAPT = [raw_time'; F0_yaapt'];
+RAPT = [raw_time'; F0_rapt'];
+MyRAPT = [raw_time'; F0_myrapt'];
 
-fileID = fopen('Reference.txt','w');
-fprintf(fileID,'%6s %12s\n','time','F0');
-fprintf(fileID,'%6.2f %12.8f\n',Reference);
+fileID = fopen(output_reference,'w+');
+fprintf(fileID,'%6.4f %12.8f\n',Reference);
 fclose(fileID);
 
-fileID = fopen('YAAPT.txt','w');
-fprintf(fileID,'%6s %12s\n','time','F0');
-fprintf(fileID,'%6.2f %12.8f\n',YAAPT);
+fileID = fopen(output_yaapt,'w+');
+fprintf(fileID,'%6.4f %12.8f\n',YAAPT);
 fclose(fileID);
 
-fileID = fopen('RAPT.txt','w');
-fprintf(fileID,'%6s %12s\n','time','F0');
-fprintf(fileID,'%6.2f %12.8f\n',RAPT);
+fileID = fopen(output_rapt,'w+');
+fprintf(fileID,'%6.4f %12.8f\n',RAPT);
 fclose(fileID);
 
-fileID = fopen('MyRAPT.txt','w');
-fprintf(fileID,'%6s %12s\n','time','F0');
-fprintf(fileID,'%6.2f %12.8f\n',MyRAPT);
+fileID = fopen(output_myrapt,'w+');
+fprintf(fileID,'%6.4f %12.8f\n',MyRAPT);
 fclose(fileID);
+end
